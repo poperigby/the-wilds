2ec2b6e647ce0e1ce1074591a22030f62f157cc6
Pope_Rigby <poperigby@mailbox.org>
2024-01-17 14:24:47 -0800

    Setup Connection struct to handle sending data to server

--- client-cli/src/main.rs
+++ client-cli/src/main.rs
@@ -1,16 +1,35 @@
-use serde_json;
+use serde::{de::DeserializeOwned, Serialize};
 use shared::Message;
-use std::{io::prelude::*, net::TcpStream};
+use std::{
+    io::prelude::*,
+    net::{TcpStream, ToSocketAddrs},
+};
 
 fn main() -> std::io::Result<()> {
-    let mut stream = TcpStream::connect("127.0.0.1:19773")?;
+    let connection = Connection::build("127.0.0.1:17732")?;
 
     let message = Message {
         message: "Hello, world!".to_string(),
         id: 0,
     };
 
-    stream.write(&serde_json::to_vec(&message).unwrap());
+    connection.send_data(&message);
 
     Ok(())
+}
+
+struct Connection {
+    stream: TcpStream,
+}
+
+impl Connection {
+    fn build<A: ToSocketAddrs>(address: A) -> std::io::Result<Self> {
+        Ok(Connection {
+            stream: TcpStream::connect(address)?,
+        })
+    }
+
+    fn send_data<T: DeserializeOwned + Serialize>(mut self, data: &T) {
+        self.stream.write(&serde_json::to_vec(&data).unwrap());
+    }
 }
