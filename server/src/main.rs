65ff1781fb5ec0ebbcea83b8be7bf5845b4f16cc
Pope_Rigby <poperigby@mailbox.org>
2024-01-17 11:34:38 -0800

    Add client-cli

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,8 @@
+use std::{
+    io::{Read, Write},
+    net::{TcpListener, TcpStream},
+};
+
 fn main() {
-    println!("Hello, world!");
+    let mut listener = TcpListener::bind("127.0.0.1:19773");
 }
