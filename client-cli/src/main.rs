2568f5a233074f35ab5c8fc2e619d3cebf75752a
Pope_Rigby <poperigby@mailbox.org>
2024-01-19 16:26:30 -0800

    Move Connection to client-lib

--- client-cli/src/main.rs
+++ client-cli/src/main.rs
@@ -1,9 +1,5 @@
-use serde::{de::DeserializeOwned, Serialize};
+use client_lib::Connection;
 use shared::Message;
-use std::{
-    io::prelude::*,
-    net::{TcpStream, ToSocketAddrs},
-};
 
 fn main() -> std::io::Result<()> {
     let connection = Connection::new("127.0.0.1:19773")?;
@@ -16,23 +12,4 @@ fn main() -> std::io::Result<()> {
     connection.send_data(&message);
 
     Ok(())
-}
-
-struct Connection {
-    stream: TcpStream,
-}
-
-/// A TCP connection to a The Wilds server
-impl Connection {
-    /// Create a new connection to a server at the given address
-    fn new<A: ToSocketAddrs>(address: A) -> std::io::Result<Self> {
-        Ok(Connection {
-            stream: TcpStream::connect(address)?,
-        })
-    }
-
-    /// Send a serializeable struct over the connection
-    fn send_data<D: DeserializeOwned + Serialize>(mut self, data: &D) {
-        self.stream.write(&serde_json::to_vec(&data).unwrap());
-    }
 }
--- client-lib/src/lib.rs
+++ client-lib/src/lib.rs
@@ -1,14 +1,25 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use serde::{de::DeserializeOwned, Serialize};
+use std::{
+    io::prelude::*,
+    net::{TcpStream, ToSocketAddrs},
+};
+
+pub struct Connection {
+    stream: TcpStream,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+/// A TCP connection to a The Wilds server
+impl Connection {
+    /// Create a new connection to a server at the given address
+    pub fn new<A: ToSocketAddrs>(address: A) -> std::io::Result<Self> {
+        Ok(Connection {
+            stream: TcpStream::connect(address)?,
+        })
+    }
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    /// Send a serializeable struct over the connection
+    pub fn send_data<D: DeserializeOwned + Serialize>(mut self, data: &D) {
+        let json_data = &serde_json::to_vec(&data).unwrap();
+        self.stream.write(json_data);
     }
 }
