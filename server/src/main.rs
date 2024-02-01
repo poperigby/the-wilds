b668dbe714d138b30b0f354ded47d0ce600d9e52
Pope_Rigby <poperigby@mailbox.org>
2024-02-01 10:34:57 -0800

    Setup human-panic

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,6 +1,9 @@
+use human_panic::setup_panic;
 use server::Server;
 
 fn main() {
+    setup_panic!();
+
     let server = Server::bind("127.0.0.1:19773").unwrap();
     server.listen().unwrap();
 }
