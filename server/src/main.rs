d2027dca6aab1caec743147f55ad7c2a5db207fe
Pope_Rigby <poperigby@mailbox.org>
2024-02-04 11:31:16 -0800

    Strip out stuff

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,11 +1,7 @@
 use async_std::main;
 use human_panic::setup_panic;
-use server::Server;
 
 #[main]
 async fn main() {
     setup_panic!();
-
-    let server = Server::bind("127.0.0.1:19773").unwrap();
-    server.listen().await.unwrap();
 }
