0a18f5e439d6ce690b3f8166f3fe2ade9c3abf7d
Pope_Rigby <poperigby@mailbox.org>
2024-02-06 16:06:56 -0800

    Axum stuff

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,7 +1,14 @@
+use axum::{routing::get, serve, Router};
 use human_panic::setup_panic;
-use tokio::main;
+use tokio::net::TcpListener;
 
-#[main]
+#[tokio::main]
 async fn main() {
     setup_panic!();
+
+    let app = Router::new().route("/", get(|| async { "Hello, world!" }));
+
+    let listener = TcpListener::bind("localhost:3000").await.unwrap();
+
+    serve(listener, app).await.unwrap();
 }
